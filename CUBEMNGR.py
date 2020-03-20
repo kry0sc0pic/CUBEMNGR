a7a599cbafa9215c1bb9c681aca0c3713ab472ff
Krishaay Jois <62088268+developer7575@users.noreply.github.com>
2020-03-20 18:13:57 +0530

    Added comments and formatted document
    Also removed test statements

--- CUBEMNGR.py
+++ CUBEMNGR.py
@@ -1,25 +1,37 @@
+# All module imports
 import shutil
 from os import mkdir
 from subprocess import call
 from tkinter import Label, filedialog, Button, Tk, Entry, Listbox
 import datetime as dt
+
+# Get the date and time
 t1 = dt.datetime.now()
 t1 = str(t1)
-date , ttime = t1.split(' ')
-time , _ = ttime.split('.')
+date, ttime = t1.split(' ')
+time, _ = ttime.split('.')
 times = []
-PDFPath = ""
 
 
-def selectPDF():
-    PDFPath = filedialog.askopenfilename(title="Select Scramble PDF", initialdir="/home/cyberdude")
-    pdfpath["text"] = PDFPath
-#    print(pdfpath)
+FILEPATH = ""  # Intialize a variable to hold the path to the Scramble file.
 
-def addTimeF():
+
+# Function to ask user for the scramble file path and display it on the UI
+def selectFile():
+    FILEPATH = filedialog.askopenfilename(
+        title="Select Scramble PDF", initialdir="")  # Add your preferred starting directory in the initialdir field
+    filepath["text"] = FILEPATH
+
+# Function to add a time entry to the listbox on the UI
+
+
+def addTimeToList():
     string = f"{SrNoEnt.get()}) {minEnt.get()}:{secEnt.get()}.{msecEnt.get()}"
     timings.insert("end", string)
     times.append(f"{minEnt.get()}:{secEnt.get()}.{msecEnt.get()}")
+
+# Function to export the solve times and other details to a .txt file inside a new folder
+
 
 def exportResults():
     timeList = time.split(':')
@@ -28,25 +40,25 @@ def exportResults():
     s = int(timeList[2])
     path = f"Results for {date} - {h}-{m}-{s}"
     mkdir(path)
-    with open(f"{path}/results.txt" , "a") as file:
+    with open(f"{path}/results.txt", "a") as file:
         file.write(f"Date: {date}\n")
         file.write(f"Time: {h}:{m}:{s}\n")
         t3 = 0
         for x in times:
             t3 += 1
             file.write(f"Attempt {t3}: {x}\n")
-        if(PDFPath):
-            shutil.copy(PDFPath , path)
-            
+        if(FILEPATH):
+            shutil.copy(FILEPATH, path)
 
-# Basic Stuff
+
+# Create the Application window and create a title label
 window = Tk()
 window.title("Cube Manager")
 title = Label(window, text="Cube Manager")
 title.grid(row=0, column=0, columnspan=4)
 
 
-# TIME ENTERING CODE
+# Code to create buttons and entry boxes to enter the solve times
 SrNo = Label(window, text="Attempt Number")
 minlab = Label(window, text="min")
 seclab = Label(window, text="sec")
@@ -56,7 +68,7 @@ minEnt = Entry(window)
 secEnt = Entry(window)
 msecEnt = Entry(window)
 timings = Listbox(window)
-addTime = Button(window, text="Add time", command=addTimeF)
+addTime = Button(window, text="Add time", command=addTimeToList)
 SrNo.grid(column=0, row=3)
 minlab.grid(column=1, row=3)
 seclab.grid(column=2, row=3)
@@ -68,20 +80,14 @@ msecEnt.grid(column=3, row=4)
 timings.grid(column=0, row=5, columnspan=4)
 addTime.grid(column=0, row=6, columnspan=4)
 
-# AVERAGES
-'''
-ao5Lab = Label(window, text="Average of 5: ")
-ao5 = Label(window, text="")
-ao5Lab.grid(column=0, row=7, columnspan=2)
-ao5.grid(column=2, row=7, columnspan=2)
-'''
-# File Selection
-selectScramPDF = Button(window, text="Select Scramble File ", command=selectPDF)
-pdfpath = Label(text="")
+# Create Button and Label to select scramble file
+selectScramPDF = Button(
+    window, text="Select Scramble File ", command=selectFile)
+filepath = Label(text="")
 selectScramPDF.grid(column=0, row=8, columnspan=1)
-pdfpath.grid(column=2, row=8, columnspan=3)
+filepath.grid(column=2, row=8, columnspan=3)
 
-#Export
-exportButton = Button(window,text="Export Results" , command=exportResults)
-exportButton.grid(row = 9 , column=1, columnspan=3)
+# Create button to export the results
+exportButton = Button(window, text="Export Results", command=exportResults)
+exportButton.grid(row=9, column=1, columnspan=3)
 window.mainloop()
